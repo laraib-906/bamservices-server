86a98a1da306f1f7018658d53d67796d010ff262
laraib-906 <laraibamna906@gmail.com>
2021-12-22 01:42:24 +0500

    added oauth service

--- src/server/api/files/files.service.ts
+++ src/server/api/files/files.service.ts
@@ -1,10 +1,10 @@
-import { Delete, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { drive_v3, google } from 'googleapis';
+import { drive_v3 } from 'googleapis';
 import { IFiles, IFilesModel } from 'src/server/types/files';
 import { PaginateResult } from 'mongoose-paginate-v2';
 import { IMongooseDeleteObject } from '../../types/response';
-import * as readline from "readline";
+import { googleOAuthService } from 'src/server/services/oauth.service';
 
 const fs = require('fs');
 const rimraf = require('rimraf');
@@ -13,23 +13,10 @@ const rimraf = require('rimraf');
 export class FilesService {
 
     private driveClient: drive_v3.Drive;
-    private SCOPES = [
-        'https://www.googleapis.com/auth/drive'
-    ];
+    
 
     constructor(@InjectModel('Files') private readonly fileSchema: IFilesModel<IFiles>) {
-        this.driveClient = this.createDriveClient(
-            process.env.GOOGLE_DRIVE_CLIENT_ID,
-            process.env.GOOGLE_DRIVE_CLIENT_SECRET,
-            process.env.GOOGLE_DRIVE_REDIRECT_URI
-        )
-        // this.createDriveClient(
-        //     process.env.GOOGLE_DRIVE_CLIENT_ID,
-        //     process.env.GOOGLE_DRIVE_CLIENT_SECRET,
-        //     process.env.GOOGLE_DRIVE_REDIRECT_URI
-        // ).then((drive) => {
-        //     this.driveClient = drive;
-        // });
+        this.driveClient = googleOAuthService.getDriveClient();
     }
 
     public async listFiles(
@@ -173,34 +160,4 @@ export class FilesService {
             }
         });
     }
-
-    private createDriveClient(clientId: string, clientSecret: string, redirectUri: string) {
-        const oAuth2Client = new google.auth.OAuth2(clientId, clientSecret, redirectUri);
-
-        const url = oAuth2Client.generateAuthUrl({
-            access_type: 'offline',
-            scope: this.SCOPES,
-        });
-        console.log(url);
-
-        // const rl = readline.createInterface({
-        //     input: process.stdin,
-        //     output: process.stdout
-        // });
-
-        // rl.question('Authorization code: ', (code) => {
-        //     console.log("CODE", code);
-        //     oAuth2Client.getToken(code, (err, tokens) => {
-        //         oAuth2Client.setCredentials(tokens);
-        //         rl.close();
-        //     });
-        // });
-        oAuth2Client.setCredentials({ refresh_token: '1//04YgMfVJ0C8a2CgYIARAAGAQSNwF-L9IrsI24_C7nuf5To_iv6oR4IEsxHtg7akKHmT_i6j0oxHXwmgnwBsyG3mJsJMfvwnDYmYc' });
-
-        return google.drive({
-            version: 'v3',
-            auth: oAuth2Client,
-        });
-    }
-
 }
